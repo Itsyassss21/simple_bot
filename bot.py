8f1166479c7f9bf270b9f56da95968fb5781c2ba
Yashwant Sahebrao Mandade <126948100+Itsyassss21@users.noreply.github.com>
2023-11-23 03:40:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import random
-
+# @itzyassss21
 def My_bot(prompt):
-    greetings = ['hi', 'hello', 'hey']
-    responses = ['How can I help you today?', 'I am here to help!', 'Nice to meet you!', 'Hello there!']
+    greetings = ["Hello",  "Good morning",  "Hey","Hi","Can you help me with a programming problem?", "I need information about your products.", "I'm feeling a bit lost, can you offer some guidance?","I have a question about your services." , " Can you tell me a joke? "]
+    responses = [ "Hi there! How can I assist you today?", "Good morning! What can I help you with?",  "Hey! What brings you here today?", 'Hello there!', "Of course! I'd be happy to help with your programming issue. Please provide me with the details.","Sure thing! I can provide you with details about our products. What specifically are you looking for?","I'm here to help! Let's talk about what's on your mind and see how I can support you.", "Ask away! I'm here to answer any questions you have about our services.", "Sure, here's a joke for you: Why don't skeletons fight each other? They don't have the guts!"]
     responses_no_answer = ['Sorry, I am unable to answer that question.', 'I am not sure how to help with that.', 'I cannot answer that.', 'I am unable to help with that topic.']
     stop_phrases = ['bye', 'goodbye', 'see you']
 
